7fb4c83bff95aa9fc225006c4695c3cfb275f6b1
F.M. Panggabean <cichiomaru@users.noreply.github.com>
2024-09-19 13:58:36 +0700

    add player movement

---
+++ Assets/Apps/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+
+    [SerializeField, Range(0, 20)] private float speed;
+    private float horizontalDirection;
+
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.constraints = RigidbodyConstraints2D.FreezeRotation;
+    }
+
+    private void Update()
+    {
+        UpdatePosition();
+    }
+
+    private void UpdatePosition()
+    {
+        Vector2 direction = rb2d.velocity;
+        direction.x = horizontalDirection * speed;
+        rb2d.velocity = direction;
+    }
+
+    public void SetHorizontalDirection(float x)
+    {
+        horizontalDirection = x;
+    }
+}
---
+++ Assets/Apps/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Player player = new Player();
+
+    private void Awake()
+    {
+        player = GetComponent<Player>();
+    }
+
+    private void Update()
+    {
+        player.SetHorizontalDirection(Input.GetAxisRaw("Horizontal"));
+    }
+}
